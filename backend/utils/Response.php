9c328b1bc03c00ee2bfafa9a028e26747fe58cfa
biruk2116 <brukbelay61@gmail.com>
2026-04-17 13:52:46 +0300

    Add header content type for JSON response in response function

--- backend/utils/Response.php
+++ backend/utils/Response.php
@@ -1,6 +1,10 @@
 <?php
 
 function response($success, $message, $data = null) {
+    if (!headers_sent()) {
+        header('Content-Type: application/json; charset=utf-8');
+    }
+
     echo json_encode([
         "success" => $success,
         "message" => $message,
