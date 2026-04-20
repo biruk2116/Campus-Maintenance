2a8119b87719551991f841112fe38f654df15e16
biruk2116 <brukbelay61@gmail.com>
2026-04-20 21:42:17 +0300

    Refactor main.jsx: Reorder imports for better organization and readability

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
+import './index.css'
+import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
